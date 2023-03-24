5626a03d4f5a9fbe94c931b98304a90ab9ce3469
tunji17 <tunjiabioye@outlook.com>
2023-03-24 08:47:28 +0200

    routing

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+
 import './App.css';
+
+function AppRoutes() {
+  const routes = useRoutes(
+    [
+      {path:'/',element:<Login/>},
+      {path:'/signup',element:<Signup/>}
+    ]
+  )
+  return routes;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppRoutes/>
+    </Router>
   );
 }
 
